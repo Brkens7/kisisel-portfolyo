3640d9fdc3e295db3df925f40ac10c04667104cb
Brkens7 <burakenesyaksi9@gmail.com>
2026-05-12 17:02:47 +0300

    resimler eklendi

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,35 +6,17 @@ export const projects: Project[] = [
     title: 'Kişisel Portfolyo Sitesi',
     description: 'Geliştirici olarak yeteneklerimi, projelerimi ve deneyimlerimi sergilemek için oluşturduğum kişisel portfolyo web sitesidir. Modern web teknolojileriyle geliştirilmiş, responsive (mobil uyumlu) ve kullanıcı dostu bir tasarıma sahiptir.',
     technologies: ['HTML', 'CSS', 'TypeScript', 'React.js', 'Node.js', 'Express', 'Tailwind CSS'],
-    image: '/images/kisisel-portfolyo.png',
-    repoUrl: 'https://github.com/aliakpoyraz/kisisel-portfolyo',
+    image: '/images/burak site.png',
+    repoUrl: 'https://github.com/brkens7/kisisel-portfolyo',
     featured: true
   },
   {
-    id: 'gemini-api-ornekleri',
-    title: 'Gemini API Örnekleri',
-    description: 'Bu proje, Ostim Teknik Üniversitesi Ostimtech GDG tarafından düzenlenen HackGDG25 hackathonu öncesinde gerçekleştirilen bir eğitim kapsamında örnek teşkil etmesi amacıyla geliştirilmiştir.',
-    technologies: ['HTML5', 'CSS', 'Javascript', 'Bootstrap', 'Node.js', 'Express.js', 'Gemini API'],
-    image: '/images/gemini-api-ornekleri.png',
-    repoUrl: 'https://github.com/aliakpoyraz/gemini-api-ornekleri',
+    id: 'yakinda',
+    title: 'Çok Yakında...',
+    description: 'Yönetim Bilişim Sistemleri ve Yapay Zeka üzerine geliştirdiğim yeni projelerim çok yakında burada olacak.',
+    technologies: ['AI', 'DSS', 'MIS'],
+    image: '/images/images.png', 
+    repoUrl: '#',
     featured: true
-  },
-  {
-    id: 'frontend-mentor-projeleri',
-    title: 'Frontend Mentor Projeleri',
-    description: 'Kendi gelişimimi görmek adına frontendmentor.io da bulunan projeleri kendimce yeniden tasarlıyorum ve sizinle paylaşıyorum.',
-    technologies: ['HTML', 'CSS', 'React', 'Tailwind CSS'],
-    image: '/images/frontend-mentor.png',
-    repoUrl: 'https://github.com/aliakpoyraz/frontend-mentor',
-    featured: true
-  },
-  {
-    id: 'one-click-block-x',
-    title: 'One Click Block X',
-    description: 'Tek tuşlamayla birlikte x platformu üzeirnde bulunan kullanıcıları engellemenize olanak sağlayan basit bir javascript chrome eklentisi, chromium tabanlı bütün tarayıcılarda çalışır.',
-    technologies: ['Javascript'],
-    image: '/images/one-click-block-x.png',
-    repoUrl: 'https://github.com/aliakpoyraz/one-click-block-x',
-    featured: false
-  },
+  }
 ];
